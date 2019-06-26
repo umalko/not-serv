7c4725ef122b926cab50c7839202d16907d31616
Vladimir Malko <mavs.vl@gmail.com>
2019-06-26 13:51:04 +0300

    separated server side to other package; implemented k8s files

---
+++ server/src/main/java/com/hyatt/notificationservice/config/SecurityConfig.java
@@ -0,0 +1,67 @@
+package com.hyatt.notificationservice.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@Profile("!test")
+@EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true, securedEnabled = true)
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    private static final String[] STATIC_REGEX_PATTERNS = {"/", "/error", "/favicon.ico", "/**/*.png", "/**/*.gif",
+            "/**/*.svg", "/**/*.jpg", "/**/*.html", "/**/*.css", "/**/*.js", "/*.html", "/webjars"};
+
+    private static final String[] SWAGGER_PATTERNS = {"/swagger-ui.html/**", "/webjars/**", "/swagger-resources/**", "/v2/api-docs/**", "/csrf"};
+
+    private static final String USER_ROLE = "USER";
+    private static final String NOOP_SECRET = "{noop}";
+    private static final String[] SOCKET_REGEX = {"/socket/**"};
+    private static final String[] WEBSOCKET_REGEX = {"/notifications/**", "/notifications"};
+
+    @Value("${app.user.name:extensionuser}")
+    private String name;
+
+    @Value("${app.user.password:extensionuserpass}")
+    private String password;
+
+    // @formatter:off
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .csrf()
+                    .disable()
+                .authorizeRequests()
+                    .antMatchers(HttpMethod.OPTIONS)
+                        .permitAll()
+                    .antMatchers(STATIC_REGEX_PATTERNS)
+                        .permitAll()
+                    .antMatchers(SWAGGER_PATTERNS)
+                        .permitAll()
+                    .antMatchers(SOCKET_REGEX)
+                        .permitAll()
+                    .antMatchers(WEBSOCKET_REGEX)
+                        .permitAll()
+                    .anyRequest()
+                        .authenticated()
+                    .and()
+                .httpBasic();
+    }
+    // @formatter:on
+
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication()
+                .withUser(name)
+                .password(NOOP_SECRET + password)
+                .roles(USER_ROLE);
+    }
+}
